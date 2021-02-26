c7a2dbfd7ac89e26aa355aaf0ab4bd97900cfd94
zidane9717 <75953740+zidane9717@users.noreply.github.com>
2021-02-26 16:27:22 +0200

    Update Chest.java

--- src/main/java/atypon/worldnav/game/entites/Chest.java
+++ src/main/java/atypon/worldnav/game/entites/Chest.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Chest implements Entity,CheckableEntity {
-
+//
     private List items;
     private String name;
     private int state;
